bc6549b83d4087b8f4063b9920ce6694021df344
Minghe Huang <h.minghe@gmail.com>
2015-01-08 20:33:35 +0800

    done with token check and token request

---
+++ authentication/Rails/server/app/controllers/application_controller.rb
@@ -0,0 +1,24 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  #protect_from_forgery with: :exception
+
+  before_action :require_token_authentication
+
+  private
+  def require_token_authentication
+    unless token_autentication
+      flash[:error] = "Please use a valid token to access"
+      render :status => 511, :json => { status: "require token to access" }
+    end
+  end
+
+  def token_autentication
+    device_uuid = params[:uuid]
+    token = params[:token]
+    if User.find_by(device_uuid: device_uuid,  token: token)
+      return true
+    end
+    return false
+  end
+end
