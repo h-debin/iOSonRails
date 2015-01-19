67ac7132b12aabaab7d261029bcaebcf23028193
Minghe Huang <h.minghe@gmail.com>
2015-01-19 23:16:50 +0800

    enable the Etag checking: fuck, it will auto call the render function

--- Rails/app/controllers/api/v1/news_controller.rb
+++ Rails/app/controllers/api/v1/news_controller.rb
@@ -2,7 +2,17 @@ module Api
   module V1
     class NewsController < ApplicationController
       def index
-        render :json => News.all
+        all_news = News.all
+        if stale?(etag: all_news)
+          render :json => News.all 
+        end
+      end
+
+      def show
+        news = News.find_by(id: params[:id])
+        if stale?(etag: news)
+          render :json => News.find_by(id: params[:id])
+        end
       end
     end
   end
