de6ab4a4194a603374eeadd172daee72cb610e33
Minghe Huang <h.minghe@gmail.com>
2015-01-21 15:10:29 +0800

    fix the circle include issue

--- iOS/iOSonRails/HTTPClient.m
+++ iOS/iOSonRails/HTTPClient.m
@@ -24,14 +24,43 @@
     return sharedHTTPClient;
 }
 
+- (NSString *)getEtag:(NSURLRequest *)request {
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES);
+    NSString *filename = [[paths objectAtIndex:0] stringByAppendingPathComponent:[NSString stringWithFormat:@"%@", request.URL]];
+    NSString *etag = [NSKeyedUnarchiver unarchiveObjectWithFile:filename];
+    return etag;
+}
+
 - (void)request:(NSString *)url parameter:(NSDictionary *)params method:(NSString *)method success:(void (^)(id))successHandler failure:(void (^)(NSError *))failureHandler {
+    NSMutableURLRequest *mutableRequest =[[AFHTTPRequestSerializer serializer]
+                                          requestWithMethod:method
+                                                  URLString:url
+                                                 parameters:params
+                                                      error:nil];
+
+    AFHTTPRequestOperation *requestOperation = [[AFHTTPRequestOperation alloc] initWithRequest:mutableRequest];
+    requestOperation.responseSerializer = [AFJSONResponseSerializer serializer];
+    [mutableRequest setValue:[[UserManager sharedUserManager] getUUID] forHTTPHeaderField: @"UUID"];
+    [mutableRequest setValue:[[UserManager sharedUserManager] getToken] forHTTPHeaderField: @"Token"];
+    [requestOperation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+        successHandler(responseObject);
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        failureHandler(error);
+    }];
+    [requestOperation start];
+}
+/**
+
     AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
     [manager.requestSerializer setValue:[[UserManager sharedUserManager] getUUID] forHTTPHeaderField: @"UUID"];
     [manager.requestSerializer setValue:[[UserManager sharedUserManager] getToken] forHTTPHeaderField: @"Token"];
+    [manager.requestSerializer setValue:etag forHTTPHeaderField:@"If-None-Match"];
+    
     if ([method isEqualToString:@"GET"]) {
         [manager GET:url parameters:params
              success:^(AFHTTPRequestOperation *operation, id responseObject) {
                  successHandler(responseObject);
+                 NSLog(@"%@",operation.response.allHeaderFields[@"Etag"]);
              }
              failure:^(AFHTTPRequestOperation *operation, NSError  *error) {
                  failureHandler(error);
@@ -54,6 +83,7 @@
         failureHandler(error);
     }
 }
+ */
 
 - (void)get:(NSString *)url parameter:(NSDictionary *)params success:(void (^)(id))successHandler failure:(void (^)(NSError *))failureHandler {
     [self request:url parameter:params method:@"GET" success:successHandler failure:failureHandler];
