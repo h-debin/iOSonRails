5ced4bcb49c52a435fc30cee1b9cb60d4076d311
Minghe Huang <h.minghe@gmail.com>
2015-04-05 07:40:51 +0800

    Englishlize the comments

--- iOS/iOSonRails/Macro/Macro.h
+++ iOS/iOSonRails/Macro/Macro.h
@@ -13,7 +13,7 @@
 #define NEWS_URL "http://api.minghe.me/api/v1/news"
 #define TOKEN_REQUEST_URL "http://api.minghe.me/api/v1/request_access_token"
 
-// 宽度和高度
+// Width, Height
 #define SCREEN_WIDTH                    [[UIScreen mainScreen] bounds].size.width
 #define SCREEN_HEIGHT                   [[UIScreen mainScreen] bounds].size.height
 #define STATUS_BAR_HEIGHT               (20.f)
@@ -21,44 +21,43 @@
 #define TAB_BAR_HEIGHT                  (49.f)
 #define CELL_DEFAULT_HEIGHT             (44.f)
 
-// 文件路径
+// Path
 #define PATH_OF_APP_HOME                NSHomeDirectory()
 #define PATH_OF_TEMP                    NSTemporaryDirectory()
 #define PATH_OF_DOCUMENT                [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0]
 #define PATH_OF_CACHES                  [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) objectAtIndex:0]
 
-// 设备信息
+// Device Infomation
 #define CURRENT_SYSTEM_VERSION          [[[UIDevice currentDevice] systemVersion] floatValue]
 #define CURRENT_LANGUAGE                ([[NSLocale preferredLanguages] objectAtIndex:0])
 #define IS_PAD                          (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
 #define IS_IPHONE                       (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone)
 
-//颜色
+// Color
 #define UIColorFromHexWithAlpha(hexValue,a) [UIColor colorWithRed:((float)((hexValue & 0xFF0000) >> 16))/255.0 green:((float)((hexValue & 0xFF00) >> 8))/255.0 blue:((float)(hexValue & 0xFF))/255.0 alpha:a]
 #define UIColorFromHex(hexValue)            UIColorFromHexWithAlpha(hexValue,1.0)
 #define UIColorFromRGBA(r,g,b,a)            [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:a]
 #define UIColorFromRGB(r,g,b)               UIColorFromRGBA(r,g,b,1.0)
 
-// 打印
-/*
 #ifdef DEBUG
-#   define DLog(fmt, ...)                NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__);
+#   define NSLog(fmt, ...)                NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__);
 #else
-#   define DLog(...)
+#   define NSLog(...)
 #endif
-*/
+/*
 #ifdef DEBUG
 #   define NSLog(...) NSLog(__VA_ARGS__)
 #else
 #   define NSLog(...)
 #endif
+*/
 
 #define LOG_BOUNDS(view)                 DLog(@"%@ bounds: %@", view, NSStringFromRect([view bounds]))
 #define LOG_FRAME(view)                  DLog(@"%@ frame: %@", view, NSStringFromRect([view frame]))
 
 #define NSStringFromBool(B)              (B ? @"YES" : @"NO")
 
-// 资源
+// Resource
 #define RESOURCE_PATH(name, ext)         [[NSBundle mainBundle] pathForResource:(name) ofType:(ext)]
 #define PNG_PATH(name)                   RESOURCE_PATH(name, @"png")
 #define JPG_PATH(name)                   RESOURCE_PATH(name, @"jpg")
@@ -66,19 +65,19 @@
 #define LOAD_IMAGE(name, ext)           [UIImage imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:name ofType:ext]]
 #define IMAGE_NAMED(name)               [UIImage imageNamed:name]
 
-// 字符串
+// String
 #define LOCAL_STRING(x)                 NSLocalizedString(x, nil)
 #define IS_NULL(x)                      (!x || [x isKindOfClass:[NSNull class]])
 #define IS_EMPTY_STRING(x)              (IS_NULL(x) || [x isEqual:@""] || [x isEqual:@"(null)"])
 #define NSStringFromInt(int)            [NSString stringWithFormat:@"%d", int]
 #define NSStringFromFloat(float)        [NSString stringWithFormat:@"%f", float]
 
-// 字体
+// Font
 #define BOLD_SYSTEM_FONT(size)          [UIFont boldSystemFontOfSize:size]
 #define SYSTEM_FONT(size)               [UIFont systemFontOfSize:size]
 #define FONT(name, size)                [UIFont fontWithName:(name) size:(size)]
 
-// view 圆角
+// View Radiu Corner
 #define VIEW_RADIUS(view, radius) \
 [view.layer setCornerRadius:(radius)]; \
 [view.layer setMasksToBounds:YES]
